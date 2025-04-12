ab60fe13bfab454f04e18da0130d7b9f6eacee44
ellazjuc3m <100530854@alumnos.uc3m.es>
2025-04-12 15:18:03 -0400

    fixed assignments schema

--- Kambaz/Assignments/schema.js
+++ Kambaz/Assignments/schema.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 const schema = new mongoose.Schema(
   {
     _id: String,
-    name: String,
+    title: String,
     description: String,
+    availableStart: String,
+    dueDate: String,
+    points: Number,
     course: { type: String, ref: "CourseModel" },
   },
   { collection: "assignments" }
